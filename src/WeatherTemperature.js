b5159c3c58645f17aeb45cc482dc5f2c93d59a59
Katherine-22 <kuznetsova.kateryn@gmail.com>
2022-11-26 23:52:09 +0200

    Displayed relevant temperature

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-    return (
+    const [unit, setUnit] = useState("metrics");
+    function showFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+
+    function showCelsius(event) {
+        event.preventDefault();
+        setUnit("celsius");
+    }
+    if (unit === "celsius") {
+         return (
         <div>
         <span className="main-temp">{Math.round(props.celsius)}</span> 
-        <span className="unit">°C</span>
+        <span className="unit">°C | <a href="/" onClick={showFahrenheit}>°F</a></span>
         </div>
     );
+    } else {
+        let fahrenheitTemp = (props.celsius * 9/5) + 32;
+        return(
+        <div>
+        <span className="main-temp">{Math.round(fahrenheitTemp)}</span> 
+        <span className="unit"><a href="/" onClick={showCelsius}>°C</a> | °F</span>
+        </div>
+        )
+    }
+   
 }
