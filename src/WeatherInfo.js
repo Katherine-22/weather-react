56cc0781f2b35bb7ffb65486a2dda408e57dce41
Katherine-22 <kuznetsova.kateryn@gmail.com>
2022-11-26 19:48:48 +0200

    Optimized the classes names

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -16,11 +17,10 @@ export default function WeatherInfo(props) {
         <img src={props.data.icon} alt={props.data.description}  />
       </div>
       <div className="col-xs-12 col-sm-5">
-        <h3>
-          <span className="main-temp">{Math.round(props.data.temperature)}</span> <span className="unit">°C</span>
-          <br />
+        <div className="main-temperature">
+          <WeatherTemperature celsius={props.data.temperature}/>
           <p className="text-capitalize main-description">{props.data.description}</p>
-        </h3>
+        </div>
       </div>
 
   
@@ -28,19 +28,19 @@ export default function WeatherInfo(props) {
         <div className="col-12 col-sm-4">
           <p>
             Humidity <br />
-            <span className="numbers">{props.data.humidity}</span>%
+            <span className="numbers">{props.data.humidity} %</span>
           </p>
         </div>
         <div className="col-12 col-sm-4">
           <p>
             Wind <br />
-            <span className="numbers">{Math.round(props.data.wind)}</span>km/h
+            <span className="numbers">{Math.round(props.data.wind)} km/h</span>
           </p>
         </div>
         <div className="col-12 col-sm-4">
           <p>
             Pressure <br />
-            <span className="numbers">{props.data.pressure}</span>mb
+            <span className="numbers">{props.data.pressure} mb</span>
           </p>
         </div>
 
